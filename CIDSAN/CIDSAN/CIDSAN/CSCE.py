877b30887d5a9cab06f78af085f2c87fcc28c827
jianyu zhou <zhoujianyu121@163.com>
2024-10-13 20:37:08 +0800

    Create CSCE.py

---
+++ CIDSAN/CIDSAN/CIDSAN/CSCE.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class CSCE(nn.Module):
+    def __init__(self, num_classes, feat_dim, class_weights, cls_num_list, max_m=0.5, s=1, reduction='mean'):
+        super(CSCE, self).__init__()
+        self.device = torch.device("cuda")
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.reduction = reduction
+        self.cls_num_list = cls_num_list
+
+        self.centers = nn.Parameter(torch.randn(num_classes, feat_dim).to(self.device))
+        self.class_weights = torch.tensor(class_weights, dtype=torch.float32).to(self.device)
+
+        m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+        m_list = m_list * (max_m / np.max(m_list))
+        m_list = torch.cuda.FloatTensor(m_list)
+        self.m_list = m_list
+        assert s > 0
+        self.s = s
+
+    def forward(self, x, target):
+
+        batch_size = x.size(0)
+        x = x.view(batch_size, -1)
+
+        centers_batch = self.centers.to(x.device)
+        target = target.to(x.device)
+
+        centers_batch = torch.index_select(centers_batch, 0, target)
+
+        distances = torch.norm(x - centers_batch, p=2, dim=1)
+
+        LHC = distances * self.class_weights[target]
+        pt = torch.exp(-LHC)
+
+        CE_loss = nn.CrossEntropyLoss(reduction='none')(x, target)
+
+        ccbl_loss = self.class_weights[target] ** (1-pt) * CE_loss
+        if self.reduction == 'mean':
+            loss = torch.mean(ccbl_loss)
+            return loss
+        elif self.reduction == 'sum':
+            loss = torch.sum(ccbl_loss)
+            return loss
+        else:
+            return ccbl_loss
+
